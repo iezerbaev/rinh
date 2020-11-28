b92e9b51cc6adc1cdc010b4c2ad18d0f4afd0ae3
nosferatu-id <kirill.abdulov2001@mail.ru>
2020-11-29 02:28:30 +0300

    networking page

---
+++ resources/js/Pages/Home/Components/User.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="user-card">
+        <div class="user-card-header">
+            <div class="user-card-header-profile">
+                <img :src="profile_photo" :alt="name">
+                <div class>
+                    <div class="user-card-header__name">{{ name }}</div>
+                    <div class="user-card-header__ninjapath">{{ ninja_path }}</div>
+                </div>
+            </div>
+            <div class="user-card-header__type">
+                {{ type }}
+            </div>
+        </div>
+        <div class="user-card-tags">
+            <div class="user-card-tags__tag" v-for="tag of tags"> {{ tag }} </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "User",
+    props: {
+        name: String,
+        profile_photo: String,
+        type: String,
+        tags: Array,
+        ninja_path: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,53 +1,21 @@
 <template>
     <app-layout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                        <form-section v-for="research in researchWork" class="mt-2">
-                            <template #title>
-                                Нир
-                            </template>
-                            <template #description>
-                                {{ research.name }}
-                            </template>
-                            <template #form>
-                                <div class="col-span-6 sm:col-span-4">
-                                    <jet-label for="firstname" :value="research.links"/>
-                                    <jet-label for="firstname" :value="research.place"/>
-                                    <jet-label for="firstname" :value="research.date"/>
-                                    <jet-label for="firstname" :value="research.type"/>
-                                    <jet-label for="firstname" :value="research.created_at"/>
-                                </div>
-                            </template>
-                        </form-section>
-                    </div>
-                </div>
+        <div class="networking">
+            <div class="users-frame">
+                <user :name="user.name"
+                      :profile_photo="user.profile_photo"
+                      :ninja_path="user.ninja_path"
+                      :type="user.type"
+                      :tags="user.tags">
+
+                </user>
             </div>
-        </div>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                        <form-section v-for="grant in grants" class="mt-2">
-                            <template #title>
-                                Грант
-                            </template>
-                            <template #description>
-                                {{ grant.name }}
-                            </template>
-                            <template #form>
-                                <div class="col-span-6 sm:col-span-4">
-                                    <jet-label for="firstname" :value="grant.links"/>
-                                    <jet-label for="firstname" :value="grant.place"/>
-                                    <jet-label for="firstname" :value="grant.date"/>
-                                    <jet-label for="firstname" :value="grant.type"/>
-                                    <jet-label for="firstname" :value="grant.price"/>
-                                    <jet-label for="firstname" :value="grant.created_at"/>
-                                </div>
-                            </template>
-                        </form-section>
-                    </div>
+            <div class="categories">
+                <div class="categories-category-frame" v-for="category of categories">
+                    <label class="categories-category-frame-label">
+                        <input class="categories-category-frame-input" type="checkbox" :value="category">
+                        <span>{{ category }}</span>
+                    </label>
                 </div>
             </div>
         </div>
@@ -55,17 +23,42 @@
 </template>
 <script>
 import AppLayout from '@/Layouts/AppLayout';
-import Profile from "@/Components/BarsComponents/Profile";
-import JetLabel from "@/Jetstream/Label";
-import FormSection from "@/Jetstream/FormSection";
+import User from "@/Pages/Home/Components/User";
 
 export default {
-    props: ['grants', 'researchWork'],
+    // props: ['users'],
     components: {
         AppLayout,
-        Profile,
-        JetLabel,
-        FormSection
+        User
     },
+    data() {
+        return {
+            user: {
+                name: 'Кирилл Абдулов',
+                profile_photo: 'https://img.icons8.com/fluent/48/000000/name.png',
+                type: 'студент',
+                ninja_path: 'веб-разработка',
+                tags: [
+                    'Дизайн', 'UI/UX','Интерфейсы','Figma','Photoshop','Adobe Illustrator','Логотипы','BrandBooks','и еще чето'
+                ]
+            },
+            categories: [
+                'Дизайн',
+                'Программирование',
+                'Микробиология',
+                'Экономика',
+                'Менеджемент',
+                'История',
+                'Аналитика',
+                'Разработка ПО',
+                'Финансы',
+                'Другое'
+            ]
+        }
+    }
 }
 </script>
+
+<style>
+@import url('../../../css/networking.css');
+</style>

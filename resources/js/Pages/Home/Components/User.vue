ba0f61c40e6ca6dd454b67f2cca9f23d3b48862c
nosferatu-id <kirill.abdulov2001@mail.ru>
2020-11-29 04:24:25 +0300

    #v.0.0.3 networking user's cards

--- resources/js/Pages/Home/Components/User.vue
+++ resources/js/Pages/Home/Components/User.vue
@@ -1,36 +1,71 @@
 <template>
     <div class="user-card">
-        <div class="user-card-header">
+        <div class="user-card-header" @click="toggleUserModal">
             <div class="user-card-header-profile">
-                <img :src="profile_photo" :alt="name">
+                <img :src="user.thumbnail" :alt="user.name">
                 <div class>
-                    <div class="user-card-header__name">{{ name }}</div>
-                    <div class="user-card-header__ninjapath">{{ ninja_path }}</div>
+                    <div class="user-card-header__name">{{ user.name }}</div>
+                    <div class="user-card-header__ninjapath">{{ user.ninja_path }}</div>
                 </div>
             </div>
             <div class="user-card-header__type">
-                {{ type }}
+                {{ user.type }}
             </div>
         </div>
         <div class="user-card-tags">
-            <div class="user-card-tags__tag" v-for="tag of tags"> {{ tag.type }} </div>
+            <div class="user-card-tags__tag" v-for="tag of user.tags"> {{ tag.type }} </div>
         </div>
+
+
+        <vodal :show="show" animation="slideLeft" @hide="show=false" :height='270' :width='600'>
+            <div class="user-card-header" @click="toggleUserModal">
+                <div class="user-card-header-profile">
+                    <img :src="user.thumbnail" width="64" :alt="user.name">
+                    <div class>
+                        <div class="user-card-header__name">{{ user.name }}</div>
+                        <div class="user-card-header__ninjapath">{{ user.ninja_path }}</div>
+                    </div>
+                </div>
+                <div class="user-card-header__type">
+                    {{ user.type }}
+                </div>
+            </div>
+            <div class="contacts">
+                <div class="contacts__title">Контактные данные</div>
+                <template v-if="user.phone_verified_at !== undefined">
+                    <div>Номер телефона: {{ user.phone }}</div>
+                </template>
+                <div>Почта: {{ user.email }}</div>
+                <div>Язык: {{ user.language == 'en' ? 'Английский' : 'Русский' }}</div>
+            </div>
+            <div class="card-footer__btn change button-active" @click="toggleUserModal">Пригласить в команду</div>
+        </vodal>
     </div>
+
 </template>
 
 <script>
+import Vodal from 'vodal';
 export default {
     name: "User",
-    props: [
-        'name',
-        'profile_photo',
-        'type',
-        'tags',
-        'ninja_path',
-    ]
+    props: ['user'],
+    components: {
+        Vodal
+    },
+    data() {
+        return {
+            show: false
+        }
+    },
+    methods: {
+        toggleUserModal() {
+            this.$data.show = !this.$data.show;
+        }
+    }
 }
 </script>
 
-<style scoped>
-
+<style>
+@import "vodal/common.css";
+@import "vodal/slide-left.css";
 </style>
--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -8,34 +8,13 @@ import AppLayout from '@/Layouts/AppLayout';
 import User from "@/Pages/Home/Components/User";
 
 export default {
-    // props: ['users'],
     components: {
         AppLayout,
         User
     },
     data() {
         return {
-            user: {
-                name: 'Кирилл Абдулов',
-                profile_photo: 'https://img.icons8.com/fluent/48/000000/name.png',
-                type: 'студент',
-                ninja_path: 'веб-разработка',
-                tags: [
-                    'Дизайн', 'UI/UX','Интерфейсы','Figma','Photoshop','Adobe Illustrator','Логотипы','BrandBooks','и еще чето'
-                ]
-            },
-            categories: [
-                'Дизайн',
-                'Программирование',
-                'Микробиология',
-                'Экономика',
-                'Менеджемент',
-                'История',
-                'Аналитика',
-                'Разработка ПО',
-                'Финансы',
-                'Другое'
-            ]
+
         }
     }
 }
